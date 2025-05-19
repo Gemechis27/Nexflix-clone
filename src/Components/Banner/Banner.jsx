d25f50bb37aff9da84ffe6c40dac64032070ff6a
Gemechis27 <game.daba19@gmail.com>
2025-05-19 13:59:23 +0300

    responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -9,12 +9,10 @@ const Banner = () => {
   useEffect(() => {
     (async () => {
       try {
-        const response = await axios.get(requests.fetchTrending); // 🔧 Fixed variable name
-       
-
+        const response = await axios.get(requests.fetchTrending);
         setMovie(
-          response.data.results[ // 🔧 Fixed `result` to `results`
-            Math.floor(Math.random() * response.data.results.length) // 🔧 Fixed `math` to `Math`
+          response.data.results[
+            Math.floor(Math.random() * response.data.results.length)
           ]
         );
       } catch (error) {
@@ -23,16 +21,16 @@ const Banner = () => {
     })();
   }, []);
 
-  function truncate  (str, n)  {
+  function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + '...' : str;
-  };
+  }
 
   return (
     <div
       className='banner'
       style={{
-        backgroundSize: 'cover', // 🔧 Fixed `=` to `:`
-        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`, // 🔧 Fixed template literal
+        backgroundSize: 'cover',
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat'
       }}
@@ -41,22 +39,18 @@ const Banner = () => {
         <h1 className='Banner_title'>
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
-      
 
-       <div className="banner_buttons">
-        <button className='banner_button play'>Play</button>
-        <button className='banner_button'>My List</button>
-       </div>
+        <div className="banner_buttons">
+          <button className='banner_button play'>Play</button>
+          <button className='banner_button'>My List</button>
+        </div>
 
-       <h1 className='banner_description'>
-        {truncate(movie?.overview, 150)}
+        <h1 className='banner_description'>
+          {truncate(movie?.overview, 150)}
         </h1>
-        <div className='banner_fadeBotton'/>
-
-      
-
       </div>
 
+      <div className='banner_fadeBotton' />
     </div>
   );
 };
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,42 +1,70 @@
-import React from 'react'
-import './header.css'
-import NetflixLogo from '../../assets/images/NetFlix1.png'
+import React, { useEffect, useState } from 'react';
+import './header.css';
+import NetflixLogo from '../../assets/images/NetFlix1.png';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className='header_outer_container'>
-        <div className='header-container'>
-            <div className='header_left-container'>
-                <ul>
-                    <li><img src={NetflixLogo} alt='NetwflixLogo' width='100'/></li>
-                    <li>Netflix</li>
-                    <li>Home</li>
-                    <li>TVShow</li>
-                    <li>Movies</li>
-                    <li>Latest</li>
-                    <li>Browser By Languages</li>
-                </ul>
-
+      <div className='header-container'>
+        <div className='header_left-container'>
+          <img src={NetflixLogo} alt='Netflix Logo' className='netflix-logo' />
+          {!isMobile ? (
+            <ul className='header_nav'>
+              <li>Home</li>
+              <li>TV Shows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>Browse by Language</li>
+            </ul>
+          ) : (
+            <div className="burger-menu" onClick={toggleMenu}>
+              <MenuIcon />
             </div>
-
-            <div className='header_Right_container'>
-                <ul>
-                    <li><SearchIcon/></li>
-                    <li><NotificationsActiveIcon/></li>
-                    <li><AccountBoxIcon/></li>
-                    <li><ArrowDropDownIcon/></li>
-                </ul>
-            </div>
-
+          )}
         </div>
-     
-    </div>
-  )
-}
 
-export default Header
+        <div className='header_Right_container'>
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsActiveIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li><ArrowDropDownIcon /></li>
+          </ul>
+        </div>
+      </div>
+
+      {isMobile && showMenu && (
+        <ul className='mobile_nav'>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+          <li>Browse by Language</li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Header;
