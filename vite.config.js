8c5d46c090c7b01f00fefaacf97a9492273fd3ce
Gemechis27 <game.daba19@gmail.com>
2025-06-29 18:37:35 +0300

    final draft

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Nexflix-clone/',  // 👈 This tells Vite where the app will be served from
+  base: '/',  // 👈 This tells Vite where the app will be served from
   plugins: [react()],
 });
 
